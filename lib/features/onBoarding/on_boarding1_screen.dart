ec1559f9832e38e9a7930699e917ed96af2b1378
mariamabdelkafy6 <mariamxcoder@gmail.com>
2025-06-08 12:28:58 +0300

    add:on_boarding1_screen

--- lib/features/onBoarding/on_boarding1_screen.dart
+++ lib/features/onBoarding/on_boarding1_screen.dart
@@ -5,6 +5,55 @@ class OnBoarding1Screen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network(
+              'https://i.postimg.cc/Gh9qGb7t/Illustartion.png',
+              height: 430,
+              width: 408,
+            ),
+            Text(
+              'Find your Comfort Food\n here',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Color(0xff09051C),
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Here You Can find a chef or dish for every\n taste and color. Enjoy!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Color(0xff09051C),
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            SizedBox(height: 42),
+            Container(
+              height: 57,
+              width: 157,
+              decoration: BoxDecoration(
+                color: Color(0xff15BE77),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(15),
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  'Next',
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
