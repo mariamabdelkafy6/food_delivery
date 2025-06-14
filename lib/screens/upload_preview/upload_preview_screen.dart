ca5c52e04f06724331b446252852ccea1d851131
mariamabdelkafy6 <mariamxcoder@gmail.com>
2025-06-14 11:22:34 +0300

    add:UpLoadPreviewScreen.

---
+++ lib/screens/upload_preview/upload_preview_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:food_delivery/screens/set_location/set_location_screen.dart';
+import 'package:food_delivery/widgets/button.dart';
+import 'package:food_delivery/widgets/left_icon.dart';
+
+class UploadPreviewScreen extends StatelessWidget {
+  const UploadPreviewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsetsDirectional.symmetric(
+          vertical: 24,
+          horizontal: 20,
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              LeftIcon(
+                title: 'Upload Your Photo\nProfile',
+              ),
+              SizedBox(height: 59.h),
+              Center(
+                child: Image.network(
+                  'https://i.postimg.cc/N0ypQVYq/Photo.png',
+                  width: 245.w,
+                  height: 238.h,
+                ),
+              ),
+              SizedBox(height: 166.h),
+              Center(
+                  child: Button(
+                title: 'Next',
+                returnScreen: SetLocationScreen(),
+              )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
