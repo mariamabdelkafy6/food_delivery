b98a3b3a702a0445e0e7418cf8ee2867425b7284
mariamabdelkafy6 <mariamxcoder@gmail.com>
2025-06-15 11:09:26 +0300

    add: SignupSuccessNotificationScreen.

---
+++ lib/screens/signup_success_notification/signup_success_notification_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:food_delivery/widgets/button.dart';
+
+class SignupSuccessNotificationScreen extends StatelessWidget {
+  const SignupSuccessNotificationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 227.h),
+              Image.network(
+                'https://i.postimg.cc/NGCyShqs/sign-up-success.png',
+                width: 172.w,
+                height: 162.h,
+              ),
+              SizedBox(height: 33.h),
+              Text(
+                'Congrats!',
+                style: TextStyle(
+                  color: Color(0xff15BE77),
+                  fontSize: 30.sp,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 12.h),
+              Text(
+                'Your Profile Is Ready To Use',
+                style: TextStyle(
+                  fontSize: 23.sp,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 192.h),
+              Button(title: 'Try Order'),
+              SizedBox(height: 60.h),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
